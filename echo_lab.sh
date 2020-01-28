cd293be827007620f59cbf316469cbb34fb698dc
Kirby127 <maxkirby1@gmail.com>
2020-01-28 09:25:29 -0500

    Added array to list each file

--- echo_lab.sh
+++ echo_lab.sh
@@ -4,9 +4,27 @@
 
 echo "My name is Max"
 
-
+#Prints the current directory
 printf "\nThe current directory is "
 pwd
+printf "\n"
 
-printf "\nThe contents of this directors is: "
-ls
+#file attribute is filled with all files in directory
+file=$(ls -a)
+
+#Declares the array
+declare -a files
+
+#Puts every word in the file variable into a different variable in the array
+for word in $file
+do
+	files+=($word)
+done
+
+#Will list out every file by number
+n=1
+for i in ${files[@]}
+do
+	echo "Number " $n "file is: " $i
+	let "n=n+1"
+done
