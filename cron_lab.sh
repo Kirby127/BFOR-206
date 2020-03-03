d6e0c2462438c0a75b813aeab0a0f9f4a207df08
Kirby127 <maxkirby1@gmail.com>
2020-03-03 09:15:17 -0500

    Changed

--- cron_lab.sh
+++ cron_lab.sh
@@ -2,10 +2,10 @@
 
 #Script that will echo output into into cron.log file.
 
-touch cron.log
+touch /var/log/cron.log
 
-echo $(date) >> cron.log
-echo "" >> cron.log
+echo $(date) "crontab was triggered" >> /var/log/cron.log
+echo "" >> /var/log/cron.log
 
 #Use crontab -e as root
 #*/2 * * * * /bin/bash /root/git/cron_lab.sh
